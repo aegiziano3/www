9d3d359907940be3f144aba867cea3e55feac41d
ariana <arianaegiziano@gmail.com>
2020-11-25 13:31:07 +1100

    Added Validation for contact for .js+contact edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var slider_img = document.querySelector('.slider-img');
-var images= ['jones_street.jpg', 'bulwara_road.jpg', 'fig_street.jpg'];
+//slideshow
+var slider_img = document.querySelector(".slider-img");
+var images= ["jones_street.jpg", "bulwara_road.jpg", "fig_street.jpg"];
 var i=0;
 
 function prev(){
@@ -15,15 +16,63 @@ function next() {
 }
 
 function setImg(index){
-    return slider_img.setAttribute('src', "Images/"+images[index]);
+    return slider_img.setAttribute("src", "Images/"+images[index]);
 }
 
 //FAQ Page
 $(document).ready(function() {
-    $('#faq').find('dd').hide().end().find('dt').click(function() {
+    $("#faq").find("dd").hide().end().find("dt").click(function() {
         $(this).next().slideToggle();
      });
 });
 
 //regular expression email
-/ (\w+)[\.\-\_]?(\w+)@(\w+)[\.](\w+)[\.]?(\w+)[\.]? /
+// / (\w+)[\.\-\_]?(\w+)@(\w+)[\.](\w+)[\.]?(\w+)[\.]? /
+
+//contact form
+
+// var form = document.getElementById ("form");
+// var email = form.email.value;
+// var valid_email = email_patt.test(email);
+// if(!valid_email) { 
+// alert
+// }
+// ​
+function forValidate() {
+var form = document.getElementById("form");
+var patt_name = /(\w+) [\.\s]?(\w?)/;
+var name = form.name.value;
+var validName = patt_name.test(name);
+
+var patt_phone = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-\/]?(\d{3})$/;
+var phone = form.mobile.value;
+var validPhone = patt_phone.test(name)
+
+var patt_email = / (\w+)[\.\-\_]?(\w+)@(\w+)\.(\w+)\.?(\w+)\.? /;
+var email = form.email.value;
+var vaildEmail = patt_email.test(email);
+
+var buy = form.bsr.value;
+    if(!validName){
+        alert(form.name.value + "is not a valid name.");
+        form.name.focus();
+        form.name.select();
+        return false;
+    }else if (!validPhone){
+        alert(form.mobile.value + "isn't a vaild phone number");
+        form.mobile.focus();
+        form.mobile.select();
+        return false;
+    }else if (!vaildEmail){
+        alert(form.email.value + "isn't a vaild email address");
+        form.email.focus();
+        form.email.select();
+        return false;
+    }
+    if (confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
+    alert("Thank you for your message");
+    return true;
+    } else {
+        return false;
+    }
+}
