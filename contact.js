9b8e415e64880086e14158d99d383ac2b4f8dbcb
ariana <arianaegiziano@gmail.com>
2020-11-27 12:21:25 +1100

    fixed jquery  <link>

--- contact.js
+++ contact.js
@@ -51,20 +51,20 @@ var buy = form.bsr.value;
 
 //Nav button
 // javascript for opening and closing navigation in mobile view
-const btn = document.querySelector('#navbutton')
+const btn = document.querySelector("#navbutton")
 // console.log( btn )
-const nav = document.querySelector('.navigation')
+const nav = document.querySelector(".navigation")
 // console.log( nav )
 
 // function to open the nav
 function onClick() {
-  if( nav.classList.contains('open') ) {
-    nav.classList.remove('open')
+  if( nav.classList.contains("open") ) {
+    nav.classList.remove("open")
   }
   else {
-    nav.classList.add('open')
+    nav.classList.add("open")
   }
 }
 
-// add a click listener to the button
-btn.addEventListener( 'click' , onClick )
+// // add a click listener to the button
+// btn.addEventListener( "click" , onClick )
