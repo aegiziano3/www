ccf3556be862d9d3b6739c39ff9cfafbcf3d64a1
ariana <arianaegiziano@gmail.com>
2020-11-30 12:07:29 +1100

    Validation updates

--- contact.js
+++ contact.js
@@ -1,3 +1,43 @@
+function formValidate () {
+  var form = document.getElementById("form");
+  var patt_name = /(\w+)[\.\s]?(\w?)/;
+  var validName = patt_name.test(form.name.value);
+
+  var patt_phone = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-/]?(\d{3})$/;
+  var validPhone = patt_phone.test(form.mobile.value);
+
+  var patt_email = /(\w+)[\.\_\-]?(\w+)@(\w+)\.(\w+)\.?(\w?)\.?(\w?)/;
+  var validEmail = patt_email.test(form.email.value);
+  
+  var name = form.name.value;
+  var phone = form.mobile.value;
+  var email = form.email.value;
+  var buy = form.bsr.value;
+
+  if(!validName) {
+      alert(form.name.value + "is not a valid name.");
+      document.getElementById("form").name.focus();
+      document.getElementById("form").name.select();
+      return false;
+  }else if (!validPhone) {
+      alert(form.mobile.value + " is not a vaild phone number");
+      document.getElementById("form").mobile.focus();
+      document.getElementById("form").mobile.select();
+      return false;
+  }else if (!validEmail) {
+      alert(form.email.value + "is not a valid email address");
+      document.getElementById("form").email.focus();
+      document.getElementById("form").email.select();
+      return false
+  }
+  if(confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
+      alert("Thank you for your enquriy");
+      return true;
+  }else{
+      return false;
+  }
+
+}
 //regular expression email
 // / (\w+)[\.\-\_]?(\w+)@(\w+)[\.](\w+)[\.]?(\w+)[\.]? /
 
@@ -10,44 +50,48 @@
 // alert
 // }
 // ​
-function forValidate() {
-var form = document.getElementById("form");
-var patt_name = /(\w+) [\.\s]?(\w?)/;
-var name = form.name.value;
-var validName = patt_name.test(name);
+// document.addEventListener("DOMContentLoaded", function(){
 
-var patt_phone = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-\/]?(\d{3})$/;
-var phone = form.mobile.value;
-var validPhone = patt_phone.test(name)
+// function formValidate() {
+// var form = document.getElementById("form");
+// var patt_name = /(\w+)[\.\s]?(\w?)/;
+// var name = form.name.value;
+// var validName = patt_name.test(name);
 
-var patt_email = / (\w+)[\.\-\_]?(\w+)@(\w+)\.(\w+)\.?(\w+)\.? /;
-var email = form.email.value;
-var vaildEmail = patt_email.test(email);
+// var patt_phone = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-\/]?(\d{3})$/;
+// var phone = form.phone.value;
+// var validPhone = patt_phone.test(name)
 
-var buy = form.bsr.value;
-    if(!validName){
-        alert(form.name.value + "is not a valid name.");
-        form.name.focus();
-        form.name.select();
-        return false;
-    }else if (!validPhone){
-        alert(form.mobile.value + "isn't a vaild mobile number");
-        form.mobile.focus();
-        form.mobile.select();
-        return false;
-    }else if (!vaildEmail){
-        alert(form.email.value + "isn't a vaild email address");
-        form.email.focus();
-        form.email.select();
-        return false;
-    }
-    if (confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
-    alert("Thank you for your message");
-    return true;
-    } else {
-        return false;
-    }
-}
+// var patt_email = /(\w+)[\.\-\_]?(\w+)@(\w+)\.(\w+)\.?(\w+)\.?/;
+// var email = form.email.value;
+// var vaildEmail = patt_email.test(email);
+
+// var buy = form.bsr.value;
+//     if(!validName){
+//         alert(form.name.value + "is not a valid name.");
+//         form.name.focus();
+//         form.name.select();
+//         return false;
+//     }else if (!validPhone){
+//         alert(form.phone.value + "isn't a vaild mobile number");
+//         form.phone.focus();
+//         form.phone.select();
+//         return false;
+//     }else if (!vaildEmail){
+//         alert(form.email.value + "isn't a vaild email address");
+//         form.email.focus();
+//         form.email.select();
+//         return false;
+//     }
+//     if (confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
+//     alert("Thank you for your message");
+//     return true;
+//     } else {
+//         return false;
+//     }
+    
+// }
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 //Nav button
 // javascript for opening and closing navigation in mobile view
