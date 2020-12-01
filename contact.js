4751219b503db739bfaeffecb4f04c71c8e40c3e
ariana <arianaegiziano@gmail.com>
2020-12-01 12:35:47 +1100

    code transfer

--- contact.js
+++ contact.js
@@ -8,7 +8,6 @@ function formValidate () {
 
   var patt_email = /(\w+)[\.\_\-]?(\w+)@(\w+)\.(\w+)\.?(\w?)\.?(\w?)/;
   var validEmail = patt_email.test(form.email.value);
-  
   var name = form.name.value;
   var phone = form.mobile.value;
   var email = form.email.value;
@@ -28,7 +27,7 @@ function formValidate () {
       alert(form.email.value + "is not a valid email address");
       document.getElementById("form").email.focus();
       document.getElementById("form").email.select();
-      return false
+      return false;
   }
   if(confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
       alert("Thank you for your enquriy");
@@ -38,77 +37,21 @@ function formValidate () {
   }
 
 }
-//regular expression email
-// / (\w+)[\.\-\_]?(\w+)@(\w+)[\.](\w+)[\.]?(\w+)[\.]? /
-
-//contact form
-
-// var form = document.getElementById ("form");
-// var email = form.email.value;
-// var valid_email = email_patt.test(email);
-// if(!valid_email) { 
-// alert
-// }
-// ​
-// document.addEventListener("DOMContentLoaded", function(){
-
-// function formValidate() {
-// var form = document.getElementById("form");
-// var patt_name = /(\w+)[\.\s]?(\w?)/;
-// var name = form.name.value;
-// var validName = patt_name.test(name);
-
-// var patt_phone = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-\/]?(\d{3})$/;
-// var phone = form.phone.value;
-// var validPhone = patt_phone.test(name)
-
-// var patt_email = /(\w+)[\.\-\_]?(\w+)@(\w+)\.(\w+)\.?(\w+)\.?/;
-// var email = form.email.value;
-// var vaildEmail = patt_email.test(email);
-
-// var buy = form.bsr.value;
-//     if(!validName){
-//         alert(form.name.value + "is not a valid name.");
-//         form.name.focus();
-//         form.name.select();
-//         return false;
-//     }else if (!validPhone){
-//         alert(form.phone.value + "isn't a vaild mobile number");
-//         form.phone.focus();
-//         form.phone.select();
-//         return false;
-//     }else if (!vaildEmail){
-//         alert(form.email.value + "isn't a vaild email address");
-//         form.email.focus();
-//         form.email.select();
-//         return false;
-//     }
-//     if (confirm("Are you sure you want to submit this form?\nYour name: " + "\n" + "Your email:" + email + "\n" + "Buy:" + buy)) {
-//     alert("Thank you for your message");
-//     return true;
-//     } else {
-//         return false;
-//     }
-    
-// }
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 //Nav button
 // javascript for opening and closing navigation in mobile view
-const btn = document.querySelector("#navbutton")
+const btn = document.querySelector("#navbutton");
 // console.log( btn )
-const nav = document.querySelector(".navigation")
+const nav = document.querySelector(".navigation");
 // console.log( nav )
 
 // function to open the nav
 function onClick() {
+  btn.addEventListener( "click" , onClick )
   if( nav.classList.contains("open") ) {
-    nav.classList.remove("open")
+    nav.classList.remove("open");
   }
   else {
-    nav.classList.add("open")
+    nav.classList.add("open");
   }
 }
-
-// // add a click listener to the button
-// btn.addEventListener( "click" , onClick )
--- faq.js
+++ faq.js
@@ -5,22 +5,19 @@ $(document).ready(function() {
      });
 });
 
-//Nav button
 // javascript for opening and closing navigation in mobile view
-const btn = document.querySelector('#navbutton')
+const btn = document.querySelector("#navbutton");
 // console.log( btn )
-const nav = document.querySelector('.navigation')
+const nav = document.querySelector(".navigation");
 // console.log( nav )
 
 // function to open the nav
 function onClick() {
-  if( nav.classList.contains('open') ) {
-    nav.classList.remove('open')
+  btn.addEventListener( "click" , onClick )
+  if( nav.classList.contains("open") ) {
+    nav.classList.remove("open");
   }
   else {
-    nav.classList.add('open')
+    nav.classList.add("open");
   }
 }
-
-// add a click listener to the button
-btn.addEventListener( 'click' , onClick )
